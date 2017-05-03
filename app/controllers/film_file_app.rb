00bb37dd4c012b3a46dd01a5e7dfacf3367e665d
Sarah Kirk <kirk710@gmail.com>
2017-05-03 17:22:40 -0600

    seeing if this goes to git

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,13 @@
 class FilmFile < Sinatra::Base
+
   get '/films' do
+    @films = Film.all
     erb :films_index
   end
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :"genres/index"
+  end
+
 end
